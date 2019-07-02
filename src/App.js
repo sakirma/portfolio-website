570873e527afb7fbdf485813ecba0b58cfc8fdd6
Huseyin Caliskan <huseyincaliskan32@gmail.com>
2019-07-02 21:00:04 +0200

    First layout is done & background is done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import React, {Component} from 'react';
+import React from 'react';
 import Grid from "@material-ui/core/Grid";
 import Avatar from "@material-ui/core/Avatar"
 import Typography from "@material-ui/core/Typography";
+import {Container, Paper} from "@material-ui/core";
 
-import logo from "./images/logo.jpeg";
+
+import withStyles from "@material-ui/core/styles/withStyles";
+
 
 import "./header.css"
-import withStyles from "@material-ui/core/styles/withStyles";
+import logo from "./images/logo.jpeg";
+import layer from "./images/overlay.png";
+import makeStyles from "@material-ui/core/styles/makeStyles";
+
 
 const BigAvatar = withStyles({
     root: {
@@ -17,41 +23,52 @@ const BigAvatar = withStyles({
 })(Avatar);
 
 
-class App extends Component {
-    render() {
-        return (
+const linearBackground = makeStyles({
+    root: {
+        backgroundImage: 'url(' + layer +') ,linear-gradient(45deg, #e37682 15%, #5f4d93 85%)',
+    }
+});
+
+function App() {
+    const backgroundClasses = linearBackground();
+    return (
+        <Container style={{flexGrow: 1, minHeight: "100vh", minWidth: "100vw"}} className={backgroundClasses.root}>
             <Grid
                 container
-                direction="column"
-                style={{border: "solid 5px", height: "100%"}}
+                spacing={0}
+                justify="center"
+                style={{height: "70vh"}}
             >
-                <Grid item style={{height: "70%", border: "solid 1px", backgroundColor: "yellow"}} container
-                      direction="column" alignItems="center" justify="center">
+                <Grid item container direction="column" xs align="center" justify="flex-end">
                     <Grid item>
                         <BigAvatar alt="Huseyin Caliskan" src={logo} className="bigAvatar"> </BigAvatar>
                     </Grid>
 
                     <Grid item>
-                        <Typography variant="h3">
+                        <Typography variant="h3" style={{color: "white"}}>
                             Huseyin Caliskan
                         </Typography>
                     </Grid>
 
-                    <Grid item>
-                        <Typography variant="subtitle1">
+                    <Grid item style={{paddingBottom: "10vh"}}>
+                        <Typography variant="subtitle1" style={{color: "white"}}>
                             <p>
                                 Just an ordinary Software Engineer
                             </p>
                         </Typography>
                     </Grid>
                 </Grid>
-
-                <Grid item xs style={{flex: 1, border: "solid 1px", backgroundColor: "red"}}>
-                    Content
-                </Grid>
             </Grid>
-        );
-    }
+
+            <Grid
+                container
+                spacing={0}
+                justify="center"
+
+            >
+            </Grid>
+        </Container>
+    );
 }
 
 export default App;
